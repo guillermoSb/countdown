08d1ff6aefd800b5a8d2607ed5e96292ce25160c
guillermoSb <guillermo.santosba@gmail.com>
2021-09-28 10:13:45 -0600

    Initial Commit

---
+++ countdown/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  countdown
+//
+//  Created by Guille on 28/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ countdown/countdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  countdownApp.swift
+//  countdown
+//
+//  Created by Guille on 28/09/21.
+//
+
+import SwiftUI
+
+@main
+struct countdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
